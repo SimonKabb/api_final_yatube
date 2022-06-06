602ad81e0e58eb25da0b926dcfb9f418f5326746
Семён Капустников <s.v.kapustnikov@gmail.com>
2022-06-07 00:50:58 +0300

    add permissions

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,41 +1,30 @@
-from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from posts.models import Follow, Group, Post
+from api.permissions import AuthorOrReadOnly
 from api.serializers import (CommentSerializer, FollowSerializer,
                              GroupSerializer, PostSerializer)
 
 
-class CreatUpdateDestroyViewSet(viewsets.ModelViewSet):
-    """Материнский класс включающий методы для проверки автора контента
-    и пагинацию"""
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
     pagination_class = LimitOffsetPagination
+    permission_classes = [AuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента невозможно')
-        super(CreatUpdateDestroyViewSet, self).perform_update(serializer)
 
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого контента невозможно')
-        super(CreatUpdateDestroyViewSet, self).perform_destroy(instance)
-
-
-class PostViewSet(CreatUpdateDestroyViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class CommentViewSet(CreatUpdateDestroyViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = [AuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
     def get_queryset(self):
         post_id = self.kwargs.get("pk_post")
